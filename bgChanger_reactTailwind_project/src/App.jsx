2f1a7b694d37b1f01d4046152c4f38ff30288736
MilanPavasiya <milpavasiya@gmail.com>
2024-06-26 15:39:07 -0400

    Complete

--- bgChanger_reactTailwind_project/src/App.jsx
+++ bgChanger_reactTailwind_project/src/App.jsx
@@ -1,12 +1,7 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import Footer from './components/Footer';
 
 function App() {
-	let colors = ['Red', 'Green', 'Blue', 'Olive', 'Gray', 'Orange'];
-
 	return (
 		<>
 			<Footer />
