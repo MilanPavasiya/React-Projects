4e1947fa55cdc2fbfbadb816e000d4482726a89b
MilanPavasiya <milpavasiya@gmail.com>
2024-06-25 22:38:45 -0400

    commit

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
 			<h1 className='bg-yellow-400 text-black p-4 rounded-xl mb-4'>
 				Tailwind and DevUI testing
 			</h1>
-			<Card channel='Chaiaurcode' someObj={myObj} someArr={newArr} />
+			<Card channel='Code with Milan' />
 			<Card />
 		</>
 	);
