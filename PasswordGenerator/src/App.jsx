1be7c364b55d1960427c411c2e20b07a5f97b7e5
MilanPavasiya <milpavasiya@gmail.com>
2024-06-28 22:44:37 -0400

    commit

--- PasswordGenerator/src/App.jsx
+++ PasswordGenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 
 function App() {
 	const [length, setLength] = useState(8);
@@ -14,11 +14,15 @@ function App() {
 		if (charAllowed) string += '!@#$%^&*()-+[]{}~`';
 
 		for (let i = 1; i < length; i++) {
-			pass = string[Math.floor(Math.random() * string.length + 1)];
+			pass += string[Math.floor(Math.random() * string.length + 1)];
 		}
 
-		setPassword(password);
+		setPassword(pass);
 	}, [length, numberAllowed, charAllowed, setPassword]);
+
+	useEffect(() => {
+		passwordGenerator();
+	}, [length, numberAllowed, charAllowed, passwordGenerator]);
 
 	return (
 		<>
@@ -26,7 +30,9 @@ function App() {
 				<h1 className='text-center text-white my-3'>Password Generator</h1>
 				<div className='flex shadow rounded-lg overflow-hidden mb-4'>
 					<input
+						id='password'
 						type='text'
+						name='password'
 						value={password}
 						className='outline-none w-full py-1 px-3'
 						placeholder='Password'
@@ -44,10 +50,11 @@ function App() {
 							min={6}
 							max={100}
 							value={length}
+							id='length'
 							className='cursor-pointer'
 							onChange={(e) => setLength(e.target.value)}
 						/>
-						<label>Length: {length}</label>
+						<label htmlFor='length'>Length: {length}</label>
 					</div>
 					<div className='flex items-center gap-x-1'>
 						<input
