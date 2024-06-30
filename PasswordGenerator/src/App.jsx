f63b8f75c10844beef342dc5c390ac061d7f6e11
MilanPavasiya <milpavasiya@gmail.com>
2024-06-30 10:18:07 -0400

    commit

--- PasswordGenerator/src/App.jsx
+++ PasswordGenerator/src/App.jsx
@@ -1,10 +1,12 @@
-import { useState, useCallback, useEffect } from 'react';
+import { useState, useCallback, useEffect, useRef } from 'react';
 
 function App() {
 	const [length, setLength] = useState(8);
 	const [numberAllowed, setNumberAllowed] = useState(false);
 	const [charAllowed, setCharAllowed] = useState(false);
 	const [password, setPassword] = useState('');
+
+	const passwordRef = useRef(null);
 
 	const passwordGenerator = useCallback(() => {
 		let pass = '';
@@ -19,6 +21,12 @@ function App() {
 
 		setPassword(pass);
 	}, [length, numberAllowed, charAllowed, setPassword]);
+
+	const copyPasswordToClipboard = useCallback(() => {
+		passwordRef.current?.select();
+		passwordRef.current?.setSelectionRange(0, 999);
+		window.navigator.clipboard.writeText(password);
+	}, [password]);
 
 	useEffect(() => {
 		passwordGenerator();
@@ -37,9 +45,11 @@ function App() {
 						className='outline-none w-full py-1 px-3'
 						placeholder='Password'
 						readOnly
+						ref={passwordRef}
 					/>
-					<button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>
-						{' '}
+					<button
+						onClick={copyPasswordToClipboard}
+						className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>
 						Copy
 					</button>
 				</div>
