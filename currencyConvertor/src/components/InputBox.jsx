656323e126e4a868a0262d8ae654863128139dcf
MilanPavasiya <milpavasiya@gmail.com>
2024-07-02 11:33:50 -0400

    commit

--- currencyConvertor/src/components/InputBox.jsx
+++ currencyConvertor/src/components/InputBox.jsx
@@ -19,6 +19,9 @@ function InputBox({
 					className='outline-none w-full bg-transparent py-1.5'
 					type='number'
 					placeholder='Amount'
+					disabled={amountDisable}
+					value={amount}
+					onChange={(e) => onAmountChange}
 				/>
 			</div>
 			<div className='w-1/2 flex flex-wrap justify-end text-right'>
