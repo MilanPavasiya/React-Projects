274fad544979326782954b0642a181271dc1fdb1
MilanPavasiya <milpavasiya@gmail.com>
2024-07-16 19:53:41 -0400

    commit

--- reactRouter/src/components/Github/Github.jsx
+++ reactRouter/src/components/Github/Github.jsx
@@ -1,20 +1,34 @@
 import React, { useEffect, useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 function Github() {
-	const [data, setData] = useState();
+	const data = useLoaderData();
+	// const [data, setData] = useState();
 
-	useEffect(() => {
-		fetch('https://api.github.com/users/milanpavasiya')
-			.then((res) => res.json())
-			.then((data) => setData(data));
-	}, []);
+	// useEffect(() => {
+	// 	fetch('https://api.github.com/users/milanpavasiya')
+	// 		.then((res) => res.json())
+	// 		.then((data) => setData(data));
+	// }, []);
 
 	return (
-		<div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>
-			Github Profile name : {data.name}
-			<img src={data.avatar_url} alt='Github picture' width={300} />
+		<div className='flex justify-center m-4 bg-gray-600 text-white p-4 text-3xl'>
+			<div>
+				GitHub Profile: {data.name}
+				<img
+					className='m-8'
+					src={data.avatar_url}
+					alt='Github picture'
+					width={300}
+				/>
+			</div>
 		</div>
 	);
 }
 
 export default Github;
+
+export const githubInfoLoader = async () => {
+	const response = await fetch('https://api.github.com/users/milanpavasiya');
+	return response.json();
+};
