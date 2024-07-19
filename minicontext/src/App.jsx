18af1ccd45183a1e8a2a6ede834a434a164e01df
MilanPavasiya <milpavasiya@gmail.com>
2024-07-19 14:29:23 -0400

    context-API config

--- minicontext/src/App.jsx
+++ minicontext/src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import Login from './components/Login';
+import Profile from './components/Profile';
+import UserContext from './context/UserContext';
+import UserContextProvider from './context/UserContextProvider';
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
 		<>
-			<h1> Context API - React</h1>
+			<UserContextProvider>
+				<h1> Context API - React</h1>
+				<Login />
+				<Profile />
+			</UserContextProvider>
 		</>
 	);
 }
