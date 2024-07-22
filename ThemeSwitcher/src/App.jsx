c8a1456166551c2b2a50c0102aa0a79eef2d3805
MilanPavasiya <milpavasiya@gmail.com>
2024-07-22 17:54:32 -0400

    configured context

--- ThemeSwitcher/src/App.jsx
+++ ThemeSwitcher/src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import useTheme, { ThemeProvider } from './contexts/Theme';
 
 function App() {
-	const [count, setCount] = useState(0);
+	return (
+		<ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
+			<div className='flex flex-wrap min-h-screen items-center'>
+				<div className='w-full'>
+					<div className='w-full max-w-sm mx-auto flex justify-end mb-4'>
+						{/* <ThemeBtn /> */}
+					</div>
 
-	return <>h1</>;
+					<div className='w-full max-w-sm mx-auto'>{/* <Card /> */}</div>
+				</div>
+			</div>
+		</ThemeProvider>
+	);
 }
 
 export default App;
