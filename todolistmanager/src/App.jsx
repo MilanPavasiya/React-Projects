64930159ff3693ef206d3cfda39c74ce96dfd32c
MilanPavasiya <milpavasiya@gmail.com>
2024-07-25 15:54:54 -0400

    commit

--- todolistmanager/src/App.jsx
+++ todolistmanager/src/App.jsx
@@ -1,11 +1,44 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { TodoProvider } from './contexts/TodoContext';
+import TodoForm from './components/TodoForm';
 
 function App() {
 	const [todos, setTodos] = useState([]);
 
-	const addTodo = () => {};
+	const addTodo = (todo) => {
+		setTodos((prevTodos) => [{ id: Date.now(), ...todo }, ...prevTodos]);
+	};
+
+	const updateTodo = (id, todo) => {
+		setTodos((prev) =>
+			prev.map((prevTodo) => (prevTodo.id === todo.id ? todo : prevTodo))
+		);
+	};
+
+	const deleteTodo = (id) => {
+		setTodos((prev) => prev.filter((prevTodo) => prevTodo.id !== id));
+	};
+
+	const toggleComplete = (id) => {
+		setTodos((prev) =>
+			prev.map((prevTodo) =>
+				prevTodo.id === id
+					? { ...prevTodo, completed: !prevTodo.completed }
+					: prevTodo
+			)
+		);
+	};
+
+	useEffect(() => {
+		const todos = JSON.parse(localStorage.getItem('todos'));
+
+		if (todos && todos.length > 0) setTodos(todos);
+	}, []);
+
+	useEffect(() => {
+		localStorage.setItem('todos', JSON.stringify(todos));
+	}, [todos]);
 
 	return (
 		<TodoProvider
