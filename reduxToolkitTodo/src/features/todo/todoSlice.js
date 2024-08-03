cc50016e21c51bb77f9958fae55cbe3651882772
MilanPavasiya <milpavasiya@gmail.com>
2024-08-02 23:39:22 -0400

    added updateTodo reducer

--- reduxToolkitTodo/src/features/todo/todoSlice.js
+++ reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -18,6 +18,13 @@ export const todoSlice = createSlice({
 		removeTodo: (state, action) => {
 			state.todos = state.todos.filter((todo) => todo.id !== action.payload);
 		},
+		updateTodo: (state, action) => {
+			state.todos = state.todos.map((todo) =>
+				todo.id === action.payload.id
+					? { ...todo, text: action.payload.text }
+					: todo.text
+			);
+		},
 	},
 });
 
